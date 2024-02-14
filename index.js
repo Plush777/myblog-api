1198a93cfa26db5afa0285fac764627830d0b5a3
Plush777 <sky11916@naver.com>
2024-02-14 18:01:10 +0900

    dsadasda

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const server = express();
-const bs = require('browser-sync').create();
 const port = 8000;
 const multer = require('multer');
 const basicAuth = require('express-basic-auth');
@@ -14,7 +13,7 @@ server.use(basicAuth({
     users,
     challenge: true,
 }));
-server.use(express.static(__dirname));
+server.use(express.static("public"));
 
 const today = new Date();
 const year = today.getFullYear();
@@ -37,30 +36,11 @@ const upload = multer({ storage });
 const fs = require('fs');
 
 server.listen(port, () => {
-    // Browsersync
-    bs.init({
-        proxy: `http://localhost:${port}`,
-        port: 8080,
-        ui: false, 
-        files: ['*.js','views/*.html'], // 감시할 파일 확장자
-        browser: 'chrome',
-        notify: true, // 브라우저 알림 
-        online: false, // 오프라인 상태 감지 
-        open: false, // 브라우저 자동 오픈  
-    });
+    console.log(`Server is running on port ${port}`);
 });
 
 server.post('/upload', upload.single('image'), (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(` <div>업로드 성공!</div>
-    <a href="/set" onclick="resetFileInput()">홈으로</a>
-
-    <script>
-    const resetFileInput = () => {
-        document.getElementById('imageInput').value = '';
-    }
-    </script>`)
+    res.end('File is uploaded');
 });
 
 server.get('/api', (req, res) => {
@@ -75,32 +55,13 @@ server.get('/api', (req, res) => {
 });
 
 server.get('/set', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`<h1>원하는 파일을 업로드하세요</h1>
-    <form action="/upload" method="post" enctype="multipart/form-data">
-        <input type="file" name="image" id="imageInput" />
-        <input type="submit" id="upload" value="업로드" />
-    </form>
-
-    <script>
-    const upload = document.getElementById('upload');
-    const imageInput = document.getElementById('imageInput');
-
-    upload.addEventListener('click', (e) => {
-        if (imageInput.files.length === 0) {
-            e.preventDefault();
-            alert('파일을 선택하세요');
-            return;
-        } 
-    });
-    </script>`)
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 });
 
 server.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end('<a href="/api">API</a>');
+    res.end('<a href="/api">/api</a>');
 });
 
 module.exports = server;
