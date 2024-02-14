c1ae1694eb371392b8850a76445dc5ee64337635
Plush777 <sky11916@naver.com>
2024-02-14 16:34:58 +0900

    ejs제거

--- server.js
+++ server.js
@@ -3,25 +3,38 @@ const server = express();
 const bs = require('browser-sync').create();
 const port = 8000;
 const multer = require('multer');
+const basicAuth = require('express-basic-auth');
+require('dotenv').config();
 
+const users = {
+    [process.env.BASIC_AUTH_USERNAME]: process.env.BASIC_AUTH_PASSWORD
+}
+
+server.use(basicAuth({
+    users,
+    challenge: true,
+}));
 server.use(express.static(__dirname));
-server.set('view engine', 'ejs');
 
 const today = new Date();
 const year = today.getFullYear();
 const month = today.getMonth() + 1;
 const date = today.getDate();
 
+const all = `${year}_${month}_${date}_`;
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, './upload'); 
     },
     filename: (req, file, cb) => {
-        cb(null, `${year}_${month}_${date}_${file.originalname}`);
+        cb(null, `${all}${file.originalname}`);
     },
 });
 
 const upload = multer({ storage });
+
+const fs = require('fs');
 
 server.listen(port, () => {
     // Browsersync
@@ -29,7 +42,7 @@ server.listen(port, () => {
         proxy: `http://localhost:${port}`,
         port: 8080,
         ui: false, 
-        files: ['server.js','views/*.ejs'], // 감시할 파일 확장자
+        files: ['*.js','views/*.html'], // 감시할 파일 확장자
         browser: 'chrome',
         notify: true, // 브라우저 알림 
         online: false, // 오프라인 상태 감지 
@@ -37,20 +50,27 @@ server.listen(port, () => {
     });
 });
 
-server.get('public/images/:imageName', (req, res) => {
-    res.sendFile(`${__dirname}/public/images/${req.params.imageName}`);
-});
-
 server.post('/upload', upload.single('image'), (req, res) => {
-    res.render('upload.ejs', { filename: req.file.originalname });
+    res.sendFile(__dirname + '/views/upload.html');
 });
 
-const production = process.env.NODE_ENV === 'production';
+server.get('/api', (req, res) => {
+    const path = './upload';
+
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+
+    fs.readdir(path, (err, items) => {
+        res.json(items);
+    });
+});
+
+server.get('/set', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
 
 server.get('/', (req, res) => {
-    res.render('index.ejs', { production });
+    res.end('<a href="/api">API</a>');
 });
 
-module.exports = {
-    server
-}
+module.exports = server;
